2a77e7995f31e9939245c48a12a73f9da6304d2f
Malinda Prabath <malindaprabath876@gmail.com>
2025-01-02 15:38:02 +0530

    finished

--- configuretions.py
+++ configuretions.py
@@ -1,10 +1,20 @@
-
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
 
-uri = "mongodb+srv://malindaprabath876:ShC5tWbROkXpcltl@crud-api.jqorm.mongodb.net/?retryWrites=true&w=majority&appName=CRUD-API"
+# Load environment variables from .env file
+load_dotenv()
 
+# Fetch the MongoDB URI from the .env file
+uri = os.getenv("MONGO_URI")
+
+if not uri:
+    raise ValueError("MONGO_URI not found in .env file")
+
+# Create MongoDB client
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-db=client.todo_db
-collection=db["todo_data"]
+# Database and collection setup
+db = client.todo_db
+collection = db["todo_data"]
--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI, APIRouter, HTTPException
 from configuretions import collection
 from database.model import Todo
 from database.schemes import all_data  # Updated import statement
+from bson import ObjectId
+from datetime import datetime
 
 app = FastAPI()
 router = APIRouter()
 
 @router.get("/")
 async def get_all_todos():
-    data = collection.find()
+    data = collection.find({"is_deleted": False})
     return all_data(data)  # Use all_data instead of all_tasks
 
 @router.post("/")
@@ -19,5 +21,34 @@ async def create_task(new_task: Todo):
 
     except Exception as e:
         return HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
+    
+@router.put("/{task_id}")
+async def update_task(task_id: str, updated_task: Todo):
+    try:
+        id = ObjectId(task_id)
+        exesting_doc = collection.find_one({"_id":id ,"is_deleted":False})
+        if not exesting_doc:
+             return HTTPException(status_code=404, detail=f"Internal Server Error: {e}")
+        updated_task.updated_at = datetime.timestamp(datetime.now())
+        resp = collection.update_one({"_id": id}, {"$set": dict(updated_task)})
+        return {"status_code": 200, "message": "Task updated successfully"}
+
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
+    
+@router.delete("/{task_id}")
+async def delete_task(task_id: str):
+    try:
+        id = ObjectId(task_id)
+        exesting_doc = collection.find_one({"_id":id ,"is_deleted":False})
+        if not exesting_doc:
+             return HTTPException(status_code=404, detail=f"Internal Server Error: {e}")
+        resp = collection.update_one({"_id": id}, {"$set": {"is_deleted": True}})
+        return {"status_code": 200, "message": "Task deleted successfully"}
+
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
+        
+        
 
 app.include_router(router)
